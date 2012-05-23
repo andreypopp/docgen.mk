1a135f67598bc5fe21ad801509be60c65c33996e
Andrey Popp <8mayday@gmail.com>
2012-05-24 01:53:54 +0400

    oops (tabs)

--- docgen.mk
+++ docgen.mk
@@ -2,20 +2,20 @@
 
 .PHONY: all
 
-SRC						= src
-BUILD					= build
-IGNORE				= .% %.mk Makefile bin/%
-IGNORE_IMPL 	= $(patsubst %,$(SRC)/%,$(IGNORE))
-SRCALL				= $(filter-out $(IGNORE_IMPL),$(shell find . -type f))
-BUILDALL			=
+SRC           = src
+BUILD         = build
+IGNORE        = .% %.mk Makefile bin/%
+IGNORE_IMPL   = $(patsubst %,$(SRC)/%,$(IGNORE))
+SRCALL        = $(filter-out $(IGNORE_IMPL),$(shell find . -type f))
+BUILDALL      =
 
 # docgen.markdown
 
-MD_SRC 				= $(filter %.md, $(SRCALL))
-MD_BUILD			= $(patsubst ./$(SRC)/%.md,./$(BUILD)/%.html,$(MD_SRC))
-MD_RENDER			= python -c "import markdown; md = markdown.Markdown(); md.convertFile('$(1)', '$(2)')"
+MD_SRC        = $(filter %.md, $(SRCALL))
+MD_BUILD      = $(patsubst ./$(SRC)/%.md,./$(BUILD)/%.html,$(MD_SRC))
+MD_RENDER     = python -c "import markdown; md = markdown.Markdown(); md.convertFile('$(1)', '$(2)')"
 
-BUILDALL			:= $(BUILDALL) $(MD_BUILD)
+BUILDALL      := $(BUILDALL) $(MD_BUILD)
 
 all: $(BUILDALL)
 
