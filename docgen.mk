d73b57850330a4bda12b62748baf0a8afbe1b041
Andrey Popp <8mayday@gmail.com>
2012-05-24 02:16:29 +0400

    copy unmatched files as-is

--- docgen.mk
+++ docgen.mk
@@ -1,4 +1,4 @@
-# docgen
+# docgen.prelude
 
 .PHONY: all
 
@@ -6,16 +6,29 @@ SRC           = src
 BUILD         = build
 IGNORE        = .% %.mk Makefile bin/%
 IGNORE_IMPL   = $(patsubst %,$(SRC)/%,$(IGNORE))
-SRCALL        = $(filter-out $(IGNORE_IMPL),$(shell find . -type f))
+SRCALL        = $(filter-out $(IGNORE_IMPL),$(shell find $(SRC) -type f))
+SRCPROCESSED  =
 BUILDALL      =
 
 # docgen.markdown
 
 MD_SRC        = $(filter %.md, $(SRCALL))
-MD_BUILD      = $(patsubst ./$(SRC)/%.md,./$(BUILD)/%.html,$(MD_SRC))
+MD_BUILD      = $(patsubst $(SRC)/%.md,$(BUILD)/%.html,$(MD_SRC))
 MD_RENDER     = python -c "import markdown; md = markdown.Markdown(); md.convertFile('$(1)', '$(2)')"
 
 BUILDALL      := $(BUILDALL) $(MD_BUILD)
+SRCPROCESSED  := $(SRCPROCESSED) $(MD_SRC)
+
+$(BUILD)/%.html: $(SRC)/%.md
+	$(info processing $<)
+	@mkdir -p $(dir $@)
+	@$(call MD_RENDER,$<,$@)
+
+# docgen.code
+
+BUILDALL      := \
+	$(BUILDALL) \
+	$(patsubst $(SRC)/%,$(BUILD)/%,$(filter-out $(SRCPROCESSED),$(SRCALL)))
 
 all: $(BUILDALL)
 
@@ -23,7 +36,7 @@ clean:
 	$(info removing $(BUILD))
 	@rm -rf $(BUILD)
 
-$(BUILD)/%.html: $(SRC)/%.md
+$(BUILD)/%: $(SRC)/%
 	$(info processing $<)
 	@mkdir -p $(dir $@)
-	@$(call MD_RENDER,$<,$@)
+	@cp $< $@
