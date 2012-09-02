2296f05541b355e955952f7cc31825b12f8ab7d7
Andrey Popp <8mayday@gmail.com>
2012-09-02 13:08:47 +0400

    docgen.jinja2.mk: remove unused macros

--- docgen.jinja2.mk
+++ docgen.jinja2.mk
@@ -1,10 +1,7 @@
 extracttpldep	= $(shell cat $1 \
 								| grep '{% \+extends' \
 								| sed -E 's/{% +extends +"([^"]+)" +%}/\1/')
-define tpldep
-$1: $(call extracttpldep, $1)
-	touch $1
-endef
+
 BUILDALL      := $(BUILDALL:%.jinja2=%)
 
 $(BUILD)/%: $(SRC)/%.jinja2
